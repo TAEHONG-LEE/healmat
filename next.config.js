a7b98b65a1c7132672e056ef72ff603238f578a1
TAEHONG-LEE <2t_hong@naver.com>
2025-12-07 23:19:47 +0900

    chore: Vercel 배포용 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/healmat' : '',
-  assetPrefix: isProd ? '/healmat/' : '',
 };
 
 module.exports = nextConfig;
